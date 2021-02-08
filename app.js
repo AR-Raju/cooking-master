a8a2bade14c0b2db6b77a11e4970a5ba4fe3bbbc
AR-Raju <asikurrahmanraju@gmail.com>
2021-02-08 11:16:26 +0600

    click update

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // searching food
-document.getElementById("searchBtn").addEventListener("click", function () {
+const searchFood = () => {
   const input = document.getElementById("searchField").value;
   let url = "";
   if (input.length == 1) {
@@ -10,63 +10,69 @@ document.getElementById("searchBtn").addEventListener("click", function () {
 
   fetch(url)
     .then((res) => res.json())
-    .then((data) => displayMeals(data));
-});
+    .then((data) => displayMeals(data.meals));
+};
 
-displayMeals = (mealsName) => {
+const displayMeals = (mealsName) => {
+  const handleException = document.getElementById("handle-exception");
+  handleException.innerText = "";
   const mealsDiv = document.getElementById("mealsDiv");
-
-  let arr = mealsName.meals;
+  mealsDiv.innerText = "";
+  let arr = mealsName;
   // handle exception input
   if (arr == null || arr.length == 0) {
-    const handleException = document.getElementById("handle-exception");
-    handleException.innerHTML = `<h1 class="text-white"> Please enter a valid food name!!! </h1>`;
+    displayError();
   } else {
     arr.forEach((meal) => {
       const mealDiv = document.createElement("div");
       mealDiv.className = "meal-div";
-
-      const mealInfo = `<div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="${meal.strMealThumb}/preview" alt="Card image cap">
-  <div class="card-body">
-    <h5>${meal.strMeal}</h5>
-    <button onclick = "displayMealDetails('${meal.strMeal}')">Details</button>
-  </div>
-</div>`;
+      const mealInfo = `<div onclick = "displayMealDetails('${meal.idMeal}')">
+        <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="${meal.strMealThumb}/preview" alt="Card image cap">
+        <div class="card-body">
+        <h5>${meal.strMeal}</h5>
+        </div>
+        </div>
+        </div>`;
       mealDiv.innerHTML = mealInfo;
       mealsDiv.appendChild(mealDiv);
     });
   }
 };
 
+//display error
+function displayError() {
+  const handleException = document.getElementById("handle-exception");
+  handleException.innerHTML = `<h1 class="text-danger"> Something went wrong! Please enter a valid food name!!! </h1>`;
+}
+
 //display meal details
-displayMealDetails = (mealName) => {
-  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`;
+const displayMealDetails = (mealID) => {
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`;
   fetch(url)
     .then((res) => res.json())
-    .then((data) => renderMealInfo(data));
+    .then((data) => renderMealInfo(data.meals[0]));
 };
 
-function renderMealInfo(meal) {
+const renderMealInfo = (meal) => {
   const mealDetailsDiv = document.getElementById("mealDetails");
-  console.log(meal);
   mealDetailsDiv.innerHTML = `
-    <div class="card" style="width: 22rem;">
-    <img class="card-img-top" src="${meal.meals[0].strMealThumb}/preview" alt="Card image cap">
+    <div class="card mx-auto mb-4" style="width: 22rem;">
+    <img class="card-img-top" src="${meal.strMealThumb}/preview" alt="Card image cap">
     <div class="card-body">
-      <h4>${meal.meals[0].strMeal}</h4>
+      <h4>${meal.strMeal}</h4>
       <h5>Ingredient</h5>
       <ul>
-        <li>${meal.meals[0].strMeasure1}, ${meal.meals[0].strIngredient1}</li>
-        <li>${meal.meals[0].strMeasure2}, ${meal.meals[0].strIngredient2}</li>
-        <li>${meal.meals[0].strMeasure3}, ${meal.meals[0].strIngredient3}</li>
-        <li>${meal.meals[0].strMeasure4}, ${meal.meals[0].strIngredient4}</li>
-        <li>${meal.meals[0].strMeasure5}, ${meal.meals[0].strIngredient5}</li>
-        <li>${meal.meals[0].strMeasure6}, ${meal.meals[0].strIngredient6}</li>
-        <li>${meal.meals[0].strMeasure7}, ${meal.meals[0].strIngredient7}</li>
+        <li>${meal.strMeasure1}, ${meal.strIngredient1}</li>
+        <li>${meal.strMeasure2}, ${meal.strIngredient2}</li>
+        <li>${meal.strMeasure3}, ${meal.strIngredient3}</li>
+        <li>${meal.strMeasure4}, ${meal.strIngredient4}</li>
+        <li>${meal.strMeasure5}, ${meal.strIngredient5}</li>
+        <li>${meal.strMeasure6}, ${meal.strIngredient6}</li>
+        <li>${meal.strMeasure7}, ${meal.strIngredient7}</li>
         
       </ul>
     </div>
   </div>
     `;
-}
+};
